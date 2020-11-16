9e70db5e1a93c16ddf859651db94272519ad94de
Demetrius <demetrius.richardson@ibm.com>
2020-11-16 09:56:07 -0500

    Added user struct and users map and userhandler

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ import (
 )
 
 var conns = make(map[int64]net.Conn)
+var users = make(map[int64]User)
+
+// User struct to hold user information
+type User struct {
+	ID       int64
+	Conn     net.Conn
+	UserName string
+}
 
 func main() {
 	server, err := net.Listen("tcp", ":8080")
@@ -26,31 +34,68 @@ func main() {
 		connID := time.Now().Unix()
 		conns[connID] = conn
 		log.Printf("%v connected. You have %d connections.", connID, len(conns))
-		go func(id int64, c net.Conn) {
-			buf := bufio.NewReader(c)
-			for {
-				fmt.Fprintf(c, "> ")
-				req, err := buf.ReadBytes(byte('\n'))
-				if err != nil {
-					if err.Error() != "EOF" {
-						log.Printf("error: %s", err)
-					}
-					break
-				}
-				body := string(req)
-				writeMessage(c, body)
-				body = strings.TrimSpace(body)
-				if body == "quit"{
-					break
-				}
-			}
-			c.Close()
-			delete(conns, id)
-			log.Printf("%v disconnected. You have %d connections.", id, len(conns))
-		}(connID, conn)
+
+		go userHandler(connID, conn)
+		go connectionHandler(connID, conn)
 	}
 }
 
 func writeMessage(c net.Conn, msg string) {
-	fmt.Fprintf(c, "You wrote: %s\n", msg)
+	fmt.Fprintf(c, "%s\n", msg)
 }
+
+func broadcastMessage(msg string) {
+	if msg == "" {
+		return
+	}
+
+	for _, conn := range conns {
+		writeMessage(conn, msg)
+	}
+}
+
+func userHandler(id int64, c net.Conn) {
+	userbuf := bufio.NewReader(c)
+	writeMessage(c, "Enter username")
+	reqbuf, err := userbuf.ReadBytes('\n')
+	if err != nil {
+		if err.Error() != "EOF" {
+			log.Printf("error: %s", err)
+		}
+		return
+	}
+	userName := string(reqbuf)
+	userName = strings.TrimSpace(userName)
+	user := User{ID: id, Conn: c, UserName: userName}
+	users[id] = user
+	writeMessage(c, fmt.Sprintf("Username: %s, ID: %d was created.", user.UserName, user.ID))
+	
+}
+
+func connectionHandler(id int64, c net.Conn) {
+	buf := bufio.NewReader(c)
+
+	for {
+		req, err := buf.ReadBytes(byte('\n'))
+		if err != nil {
+			if err.Error() != "EOF" {
+				log.Printf("error: %s", err)
+			}
+			break
+		}
+		body := string(req)
+		msg := fmt.Sprintf("%d wrote %s", id, body)
+		broadcastMessage(msg)
+		body = strings.TrimSpace(body)
+		if body == "quit" {
+			break
+		}
+	}
+	c.Close()
+	delete(conns, id)
+	broadcastMessage(fmt.Sprintf("Connection: %v, ended.", id))
+	log.Printf("%v disconnected. You have %d connections.", id, len(conns))
+}
+
+// commandParser - Parse commands and call a function don't broadcast commands but text
+// Build user profile
